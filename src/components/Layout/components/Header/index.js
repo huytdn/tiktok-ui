615b21807e2d1b4fea0c92feeca25c9f71133fe2
huytdn <saolailatoivay991@gmail.com>
2025-04-22 18:22:56 +0700

    header-1-(logo&search)

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,30 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '@/assets/images/index';
+import icons from '@/ultis/icons';
+
+const { LuSearch, IoCloseCircle, AiOutlineLoading3Quarters } = icons;
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <IoCloseCircle />
+                    </button>
+                    <AiOutlineLoading3Quarters className={cx('loading')} />
+
+                    <button className={cx('search-btn')}>
+                        <LuSearch />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
---
+++ src/ultis/icons.js
@@ -0,0 +1,11 @@
+import { LuSearch } from 'react-icons/lu';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+import { IoCloseCircle } from 'react-icons/io5';
+
+const icons = {
+    LuSearch,
+    AiOutlineLoading3Quarters,
+    IoCloseCircle,
+};
+
+export default icons;
